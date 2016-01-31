6ae21b096d26899893806a46771fc68d47ed7eae
Raphael MC COMAS <rmc-coma@e1r1p14.42.fr>
2016-01-31 08:04:03 +0100

    Correction

--- m_mat4pro.c
+++ m_mat4pro.c
@@ -6,7 +6,7 @@
 /*   By: rmc-coma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/31 07:27:21 by rmc-coma          #+#    #+#             */
-/*   Updated: 2016/01/31 07:32:50 by rmc-coma         ###   ########.fr       */
+/*   Updated: 2016/01/31 08:03:23 by rmc-coma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ t_mat4	*m_mat4pro(T_MATU angle, T_MATU ratio, T_MATU near, T_MATU far)
 	matrix->mat[1][1] = 1 / (tan(angle / 2));
 	matrix->mat[2][2] = -(far + near) / (far - near);
 	matrix->mat[2][3] = -(2 * far * near) / (far - near);
-	matrix->mat[3][3] = -(2 * far * near) / (far - near);
+	matrix->mat[3][3] = -1;
 	return (matrix);
 }
